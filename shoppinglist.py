16d11c84c9664cb7b6c8111ade93ffb62bb92002
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-10 23:19:03 +0200

    Feature: mai() hinzugefügt

--- shoppinglist.py
+++ shoppinglist.py
@@ -32,8 +32,6 @@ def add_item(name, amount, price):
     except sqlite3.Error as e:
         print(f'Failed to add a new record: {e}')
     
-# Aufruf der Funktion, um ein Lebensmittels hinzuzufügen
-# add_item('Milch', '2 Packung', 2)
 
 # Auslesen der kompletten Einkaufsliste
 def show_item():
@@ -69,10 +67,47 @@ def delete_item(id):
         print(f'Failed to delete the item with id {id} {e}')
         
         
-
-update_item(1, 'Eier', '3 Packung', 5)
-delete_item(3)      
-show_item()
+def main():
+    while True:
+        print('<<<<<<<<<<-----Einkaufsliste----->>>>>>>>>>')
+        print('1. Artikel hinzufügen')
+        print('2. Artikel anzeigen')
+        print('3. Artikel aktualisieren')
+        print('4. Artikel löschen')
+        print('5. Program beenden')
+        
+        choice = input('Bitte wähle eine Option {1, 2, 3, 4, oder 5}: ')
+        
+        match choice:
+            case '1':
+                print('Bitte gib die Daten des neuen Artikel ein: ')
+                name = input('name: ')
+                amount = input('amount: ')
+                price = input('price: ')
+                add_item(name, amount, price)
+            case '2':
+                show_item()
+            case '3':
+                print('Bitte gib die aktualisierten Daten ein: ')
+                id = input("id: ")
+                name = input('name: ')
+                amount = input('amount: ')
+                price = input('price: ')
+                update_item(id, name, amount, price)
+            case '4':
+                print('Bitte gib die ID des zu löschonden Artikel: ')
+                id = input('id: ')
+                delete_item(id)
+            case '5':
+                print('Programm wird beended, Auf Wiedersehen')
+                break
+            case _:
+                print('Ungültige Eingabe! Bitte wähle 1, 2, 3, 4, oder 5.')
+                
+if __name__  == '__main__':
+    main()
+        
+        
 
 
 
