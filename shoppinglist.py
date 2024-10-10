ba4270e2e075355d1d2a2fbe1b0e421b0ace14da
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-10 15:51:42 +0200

    Feature: create_table() wird implementiert

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,18 +1,37 @@
-shoppinglist = []
+import sqlite3
 
-# shoppinglist.py
+# Verbindung zu SQLite (Falls nicht vorhanden, dann wird erstellt)
+conn = sqlite3.connect('shoppinglist.db')
 
-# List to store shopping items
-shoppinglist = []
+# Erstellung von cursor object, um SQL-Befehl durchzuführen
+cursor = conn.cursor()
 
-# Function to add an item to the shopping list
-def add_item():
-    # Ask the user for an item
-    item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: ")
-    # Add the item to the shopping list
-    shoppinglist.append(item)
-    # Print a confirmation message
-    print(f"{item} wurde der Einkaufsliste hinzugefügt.")
+# Erstellung von Tabellen in shoppinglist.db mit der richtegen Spalte.
+def create_table():
+    try:
+        cursor.execute('''CREATE TABLE IF NOT EXISTS groceries (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(32),
+        amount VARCHAR(32),
+        price INTEGER
+    
+        )''')
+        conn.commit()
+    except sqlite3.Error as e:
+        print(e)
+create_table()
 
-# Call the function to test
-add_item()
+# # List to store shopping items
+# shoppinglist = []
+
+# # Function to add an item to the shopping list
+# def add_item():
+#     # Ask the user for an item
+#     item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: ")
+#     # Add the item to the shopping list
+#     shoppinglist.append(item)
+#     # Print a confirmation message
+#     print(f"{item} wurde der Einkaufsliste hinzugefügt.")
+
+# # Call the function to test
+# add_item()
